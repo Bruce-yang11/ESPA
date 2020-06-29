186606e74eb58f989387c9ced8bbeae1f09ba952
Bruce-yang11 <1820486589@qq.com>
2020-06-29 11:26:27 +0800

    dasdas

--- test1/test1/Form1.cs
+++ test1/test1/Form1.cs
@@ -23,6 +23,7 @@ namespace test1
             MessageBox.Show("git test123");
             Form1 fr1 = new Form1();
             fr1.Text="同步测试"+now();
+             MessageBox.Show("git test123213");
 
         }
     }
